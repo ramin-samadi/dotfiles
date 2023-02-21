76be348cbc189b296c91cb1cb7196897c9030ea9
Ramin Samadi <ramin.samadi@elev.ga.ntig.se>
2023-02-21 01:18:52 +0100

    Create 2install.sh

---
+++ 2install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+sudo pacman -Syu xorg
+
+git clone https://aur.archlinux.org/st.git ~/bin && cd ~/bin/ && makepkg -sif –clean && cp config.def.h config.h
+git clone https://aur.archlinux.org/dwm.git ~/bin/dwm && cd ~/bin/dwm/ && makepkg -sif –clean
+
+echo '
+#!/bin/sh
+userresources=$HOME/.Xresources
+usermodmap=$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/.Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+
+# merge in defaults and keymaps
+
+if [ -f $sysresources ]; then
+    xrdb -merge $sysresources
+fi
+
+if [ -f $sysmodmap ]; then
+    xmodmap $sysmodmap
+fi
+
+if [ -f "$userresources" ]; then
+    xrdb -merge "$userresources"
+fi
+
+if [ -f "$usermodmap" ]; then
+    xmodmap "$usermodmap"
+fi
+
+# start some nice programs
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+
+setxkbmap se &
+
+# Login Loop
+while true; do
+        dwm >/dev/null 2>&1
+done
+
+# Start DWM
+exec dwm
+' > ~/.xinitrc
+
+echo '
+#
+# ~/.bash_profile
+#
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+startx
+' > ~/.bash_profile && source ~/.bash_profile
