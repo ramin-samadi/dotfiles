70c9b49aff20e7de1dd7587a4bf623359b9614b8
Ramin Samadi <ramin.samadi@elev.ga.ntig.se>
2023-02-21 23:37:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -159,6 +159,7 @@ echo '
                         { SUPER,                        XK_w,      spawn,          SHCMD("firefox") }, //Browser
                         { SUPER,                        XK_t,      spawn,          SHCMD("st") }, //Terminal
                         { SUPER,                        XK_s,      spawn,          SHCMD("dmenu_run") }, //Search
+                        { SUPER,                        XK_m,      spawn,          SHCMD("spotify") }, //Music
 
                 // █▀█ ▄▀█ █▄░█ █▀▀ █░░
                 // █▀▀ █▀█ █░▀█ ██▄ █▄▄
