0b50c95f4ea550a0dbddcc50532aeb6ce1bbe3a2
Ramin Samadi <ramin.samadi@elev.ga.ntig.se>
2023-02-23 14:13:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu wget pamixer playerctl firefox unzip xorg nodejs tldr lsd flatp
 
 git clone https://aur.archlinux.org/paru.git ~/paru && cd ~/paru/ && makepkg -sif –clean
 git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay/ && makepkg -sif –clean
-paru -Syu appimagelauncher spotify rustdesk-bin motrix-bin amdguid-glow-bin vulkan-amdgpu-pro lib32-vulkan-amdgpu-pro
+paru -Syu appimagelauncher calcurse spotify rustdesk-bin motrix-bin amdguid-glow-bin vulkan-amdgpu-pro lib32-vulkan-amdgpu-pro
 git clone https://aur.archlinux.org/paleofetch-git.git ~/paleofetch && cd ~/paleofetch/ && makepkg -sif –clean
 
 cd && wget https://github.com/raminsamadi123/hyprinstall/releases/download/Fonts/Meslo-fonts.zip
