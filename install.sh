b973ff91ebd6a8070c6072e08f3839c53f3d7a12
Ramin Samadi <ramin.samadi+github@elev.ga.ntig.se>
2023-03-11 12:53:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -213,8 +213,8 @@ done
 
 #_____________________________________________________________________________
 
-if [$USER == "ramin"] then
-    sudo pacman -Syu gnupg mpg123 python-pip pass pass-otp zbar wget pavucontrol pamixer playerctl unzip xorg nodejs tldr lsd flatpak ncdu btop bash-completion traceroute tree trash-cli cronie vi linux-headers electron cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info mesa-utils wine gnutls lib32-gnutls libpulse lib32-libpulse
+if  [[ $USER == "ramin" ]]; then
+    sudo pacman -Syu gnupg mpg123 python-pip pass-otp zbar wget pavucontrol pamixer playerctl unzip xorg nodejs tldr lsd flatpak ncdu btop bash-completion traceroute tree trash-cli cronie vi linux-headers electron cairo gtk3 gobject-introspection desktop-file-utils xdg-utils xdg-user-dirs gtk-update-icon-cache shared-mime-info mesa-utils wine gnutls lib32-gnutls libpulse lib32-libpulse
 
     pip install neovim langdetect shell-gpt --user
     cd ~/ && git clone https://aur.archlinux.org/python-gtts.git && cd python-gtts/ && makepkg -sif --clean
