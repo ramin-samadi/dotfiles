198f08a021f2ce405f46cba9b4be887f46e4e7f1
Ramin Samadi <ramin.samadi+github@elev.ga.ntig.se>
2023-03-21 15:56:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,254 +1,56 @@
 #!/bin/bash
 
-#_____________________________________________________________________________
-git clone https://aur.archlinux.org/smenu.git ~/.config/smenu && cd ~/.config/smenu && makepkg -sif –clean && clear
+aur=paru
 
-echo -e "\e[32m
-░█████╗░██╗░░░██╗██████╗░  ██╗░░██╗███████╗██╗░░░░░██████╗░███████╗██████╗░
-██╔══██╗██║░░░██║██╔══██╗  ██║░░██║██╔════╝██║░░░░░██╔══██╗██╔════╝██╔══██╗
-███████║██║░░░██║██████╔╝  ███████║█████╗░░██║░░░░░██████╔╝█████╗░░██████╔╝
-██╔══██║██║░░░██║██╔══██╗  ██╔══██║██╔══╝░░██║░░░░░██╔═══╝░██╔══╝░░██╔══██╗
-██║░░██║╚██████╔╝██║░░██║  ██║░░██║███████╗███████╗██║░░░░░███████╗██║░░██║
-╚═╝░░╚═╝░╚═════╝░╚═╝░░╚═╝  ╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝
+git clone https://aur.archlinux.org/yay.git ~/.config/yay && cd ~/.config/yay/ && makepkg -sif –clean
+git clone https://aur.archlinux.org/paru-bin.git ~/.config/paru && cd ~/.config/paru/ && makepkg -sif –clean
 
-Which aur helpers do you want to install?
-\e[0m"
-options=(yay paru-bin pakku aurutils trizen pikaur aura)
-
-selected_options=()
-
-while true; do
-    remaining_options=("Next" "${options[@]}")
-
-    for selected in "${selected_options[@]}"; do
-        remaining_options=("${remaining_options[@]/$selected}")
-    done
-
-    selected=$(printf "%s\n" "${remaining_options[@]}" | smenu -c -N "Select which aur helper to install:")
-
-    if [ "$selected" == "Next" ]; then
-        clear && break
-    fi
-
-    selected_options+=("$selected")
-done
-
-for option in "${selected_options[@]}"; do
-    git clone https://aur.archlinux.org/$option.git ~/$option && cd ~/$option/ && makepkg -sif –clean && clear
-    aur=$option
-    if [ "$aur" == "paru-bin" ]; then
-        aur=paru
-    fi
-done
-
-install() {
-    options=("$@")
-    selected_options=()
-
-    while true; do
-        remaining_options=("Next" "${options[@]}")
-
-        for selected in "${selected_options[@]}"; do
-            remaining_options=("${remaining_options[@]/$selected}")
-        done
-
-        selected=$(printf "%s\n" "${remaining_options[@]}" | smenu -c -N "Select which to install:")
-
-        if [ "$selected" == "Next" ]; then
-            clear && break
-        fi
-
-        selected_options+=("$selected")
-    done
-
-    for option in "${selected_options[@]}"; do
-        $aur -Syu "$option" && clear
-        
-        if [ "$option" == "pass" ]; then
-            $aur -Syu gnupg pass-otp zbar xclip git-remote-gcrypt passmenu-otp-git && clear
-        elif [ "$option" == "qemu-full" ]; then
-            $aur -Syu dmidecode virt-manager ebtables iptables virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat powershell-bin && clear
-        fi
-    done
-}
-
-#_____________________________________________________________________________
-
-clear
-echo -e "\e[32m
-██████╗░██████╗░░█████╗░░██╗░░░░░░░██╗░██████╗███████╗██████╗░
-██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔════╝██╔══██╗
-██████╦╝██████╔╝██║░░██║░╚██╗████╗██╔╝╚█████╗░█████╗░░██████╔╝
-██╔══██╗██╔══██╗██║░░██║░░████╔═████║░░╚═══██╗██╔══╝░░██╔══██╗
-██████╦╝██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░██████╔╝███████╗██║░░██║
-╚═════╝░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═════╝░╚══════╝╚═╝░░╚═╝
-
-Which browsers do you want to install?
-\e[0m"
-install firefox librewolf-bin vivaldi google-chrome chromium epiphany microsoft-edge-stable-bin brave-bin waterfox-classic-bin qutebrowser opera icecat
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-████████╗███████╗██████╗░███╗░░░███╗██╗███╗░░██╗░█████╗░██╗░░░░░
-╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║████╗░██║██╔══██╗██║░░░░░
-░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║██╔██╗██║███████║██║░░░░░
-░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║██║╚████║██╔══██║██║░░░░░
-░░░██║░░░███████╗██║░░██║██║░╚═╝░██║██║██║░╚███║██║░░██║███████╗
-░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝
-
-Which terminals do you want to install?
-\e[0m"
-install st alacritty yakuake terminator guake tilda tilix terminology wezterm xterm cool-retro-term gnome-console gnome-terminal konsole xfce4-terminal lxterminal
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-███████╗██████╗░██╗████████╗░█████╗░██████╗░
-██╔════╝██╔══██╗██║╚══██╔══╝██╔══██╗██╔══██╗
-█████╗░░██║░░██║██║░░░██║░░░██║░░██║██████╔╝
-██╔══╝░░██║░░██║██║░░░██║░░░██║░░██║██╔══██╗
-███████╗██████╔╝██║░░░██║░░░╚█████╔╝██║░░██║
-╚══════╝╚═════╝░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
-
-Which editors do you want to install?
-\e[0m"
-install neovim-nightly-bin vi vim emacs neovim nano visual-studio-code-bin vscodium-bin gedit notepadqq kate leafpad code
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-██╗░░░██╗██╗██████╗░████████╗██╗░░░██╗░█████╗░██╗░░░░░██╗███████╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
-██║░░░██║██║██╔══██╗╚══██╔══╝██║░░░██║██╔══██╗██║░░░░░██║╚════██║██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
-╚██╗░██╔╝██║██████╔╝░░░██║░░░██║░░░██║███████║██║░░░░░██║░░███╔═╝███████║░░░██║░░░██║██║░░██║██╔██╗██║
-░╚████╔╝░██║██╔══██╗░░░██║░░░██║░░░██║██╔══██║██║░░░░░██║██╔══╝░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║
-░░╚██╔╝░░██║██║░░██║░░░██║░░░╚██████╔╝██║░░██║███████╗██║███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║
-░░░╚═╝░░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
-
-██████╗░██╗░░░░░░█████╗░████████╗███████╗░█████╗░██████╗░███╗░░░███╗
-██╔══██╗██║░░░░░██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗████╗░████║
-██████╔╝██║░░░░░███████║░░░██║░░░█████╗░░██║░░██║██████╔╝██╔████╔██║
-██╔═══╝░██║░░░░░██╔══██║░░░██║░░░██╔══╝░░██║░░██║██╔══██╗██║╚██╔╝██║
-██║░░░░░███████╗██║░░██║░░░██║░░░██║░░░░░╚█████╔╝██║░░██║██║░╚═╝░██║
-╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝
-
-Which virtualization platforms do you want to install?
-\e[0m"
-install qemu-full virtualbox quickemu quickgui-bin
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░
-██╔══██╗██╔══██╗██╔════╝██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗
-██████╔╝███████║╚█████╗░╚█████╗░░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║
-██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗░░████╔═████║░██║░░██║██╔══██╗██║░░██║
-██║░░░░░██║░░██║██████╔╝██████╔╝░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝
-╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░
-
-███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
-████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
-██╔████╔██║███████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝
-██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
-██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
-╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
-
-What password manager do you want to install?
-\e[0m"
-install pass keepassxc bitwarden lastpass 1password seahorse
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-██████╗░███████╗███╗░░░███╗░█████╗░████████╗███████╗  ██████╗░███████╗░██████╗██╗░░██╗████████╗░█████╗░██████╗░
-██╔══██╗██╔════╝████╗░████║██╔══██╗╚══██╔══╝██╔════╝  ██╔══██╗██╔════╝██╔════╝██║░██╔╝╚══██╔══╝██╔══██╗██╔══██╗
-██████╔╝█████╗░░██╔████╔██║██║░░██║░░░██║░░░█████╗░░  ██║░░██║█████╗░░╚█████╗░█████═╝░░░░██║░░░██║░░██║██████╔╝
-██╔══██╗██╔══╝░░██║╚██╔╝██║██║░░██║░░░██║░░░██╔══╝░░  ██║░░██║██╔══╝░░░╚═══██╗██╔═██╗░░░░██║░░░██║░░██║██╔═══╝░
-██║░░██║███████╗██║░╚═╝░██║╚█████╔╝░░░██║░░░███████╗  ██████╔╝███████╗██████╔╝██║░╚██╗░░░██║░░░╚█████╔╝██║░░░░░
-╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝  ╚═════╝░╚══════╝╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░░░░
-
-Which remote desktop softwares do you want to install?
-\e[0m"
-install x11vnc rustdesk-bin teamviewer anydesk-bin remmina parsec-bin realvnc-vnc-viewer nomachine
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-███████╗██╗██╗░░░░░███████╗  ███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
-██╔════╝██║██║░░░░░██╔════╝  ████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
-█████╗░░██║██║░░░░░█████╗░░  ██╔████╔██║███████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝
-██╔══╝░░██║██║░░░░░██╔══╝░░  ██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
-██║░░░░░██║███████╗███████╗  ██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
-╚═╝░░░░░╚═╝╚══════╝╚══════╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
-
-Which file managers do you want to install?
-\e[0m"
-install ranger nautilus dolphin krusader nemo thunar caja konqueror pcmanfm xplr worker vifm
-
-#_____________________________________________________________________________
-
-echo -e "\e[32m
-░█████╗░░█████╗░██╗░░░░░███████╗███╗░░██╗██████╗░░█████╗░██████╗░
-██╔══██╗██╔══██╗██║░░░░░██╔════╝████╗░██║██╔══██╗██╔══██╗██╔══██╗
-██║░░╚═╝███████║██║░░░░░█████╗░░██╔██╗██║██║░░██║███████║██████╔╝
-██║░░██╗██╔══██║██║░░░░░██╔══╝░░██║╚████║██║░░██║██╔══██║██╔══██╗
-╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝██║░░██║██║░░██║
-░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝
-
-Which calendars do you want to install?
-\e[0m"
-install calcurse korganizer deepin-calendar nextcloud-app-calendar gcalcli
-
-#_____________________________________________________________________________
-
-if  [[ $USER == "ramin" ]]; then
-    $aur -Syu --needed - < ~/dwm/packages.txt
+$aur -Syu --needed - < ~/dwm/packages.txt
     
-    pip install neovim langdetect shell-gpt --user
+pip install neovim langdetect shell-gpt --user
 
-    gem install neovim && sudo npm install -g neovim 
-    sudo gpasswd -a $USER plugdev
-    sudo systemctl enable --now libvirtd.service && sudo usermod -a -G libvirt $(whoami) && sudo systemctl restart libvirtd.service
-    sudo virsh net-start default
-    sudo virsh net-autostart default
-    sudo systemctl restart libvirtd.service
+gem install neovim && sudo npm install -g neovim 
+sudo gpasswd -a $USER plugdev
+sudo systemctl enable --now libvirtd.service && sudo usermod -a -G libvirt $(whoami) && sudo systemctl restart libvirtd.service
+sudo virsh net-start default
+sudo virsh net-autostart default
+sudo systemctl restart libvirtd.service
 
-    git clone --depth=1 https://gitlab.com/brinkervii/grapejuice.git /tmp/grapejuice && cd /tmp/grapejuice && ./install.py
-    cd && wget https://github.com/raminsamadi123/hyprinstall/releases/download/Fonts/Meslo-fonts.zip && unzip Meslo-fonts.zip && sudo rm -rf Meslo-fonts.zip
-    git clone https://github.com/christitustech/mybash ~/mybash && cd ~/mybash/ && ./setup-arch.sh
-    sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
-    sudo ufw enable && sudo systemctl enable --now ufw.service
+git clone --depth=1 https://gitlab.com/brinkervii/grapejuice.git /tmp/grapejuice && cd /tmp/grapejuice && ./install.py
+cd && wget https://github.com/raminsamadi123/hyprinstall/releases/download/Fonts/Meslo-fonts.zip && unzip Meslo-fonts.zip && sudo rm -rf Meslo-fonts.zip
+git clone https://github.com/christitustech/mybash ~/mybash && cd ~/mybash/ && ./setup-arch.sh
+sudo pacman -Syu --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+sudo ufw enable && sudo systemctl enable --now ufw.service
 
-    cd
-    git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-    make -C ble.sh install PREFIX=~/.local
-    echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
+cd
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 
-    sudo sed -i 's/#Color/Color/g' /etc/pacman.conf && sudo sed -i 's/#NoProgressBar/ILoveCandy/g' /etc/pacman.conf && sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
-    sudo sed -i 's/ls -Fls/lsd -Fla/g' ~/.bashrc && sudo sed -i 's/ls -aFh/lsd -aFh/g' ~/.bashrc && sed -i 's,/usr/bin/grep $GREP_OPTIONS,grep --color,g' ~/.bashrc
+sudo sed -i 's/#Color/Color/g' /etc/pacman.conf && sudo sed -i 's/#NoProgressBar/ILoveCandy/g' /etc/pacman.conf && sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+sudo sed -i 's/ls -Fls/lsd -Fla/g' ~/.bashrc && sudo sed -i 's/ls -aFh/lsd -aFh/g' ~/.bashrc && sed -i 's,/usr/bin/grep $GREP_OPTIONS,grep --color,g' ~/.bashrc
     
-    mkdir ~/GitHub
+mkdir ~/GitHub
 
-    git clone https://aur.archlinux.org/st.git ~/.config/st && cd ~/.config/st/ && makepkg -sif –clean && cp config.def.h config.h
-    git clone https://aur.archlinux.org/dwm.git ~/.config/dwm && cd ~/.config/dwm/ && makepkg -sif –clean
+git clone https://aur.archlinux.org/st.git ~/.config/st && cd ~/.config/st/ && makepkg -sif –clean && cp config.def.h config.h
+git clone https://aur.archlinux.org/dwm.git ~/.config/dwm && cd ~/.config/dwm/ && makepkg -sif –clean
 
-    git clone https://github.com/LazyVim/starter ~/.config/nvim
-    rm -rf ~/.config/nvim/.git
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
     
-    rm -rf ~/.local/bin/*
-    mv ~/dwm/home/.local/bin/* ~/.local/bin/
-    mv ~/dwm/home/.xinitrc ~/
-    mv -f ~/dwm/home/.bash_profile ~/
-    mv -f ~/dwm/home/.bashrc ~/.bashrc
-    mv -f ~/dwm/home/.config/dwm/config.h ~/.config/dwm/ && cd ~/.config/dwm/ && makepkg -sif --clean
-    sudo rm -rf ~/.bashrc.bak
-    cd
+rm -rf ~/.local/bin/*
+mv ~/dwm/home/.local/bin/* ~/.local/bin/
+mv ~/dwm/home/.xinitrc ~/
+mv -f ~/dwm/home/.bash_profile ~/
+mv -f ~/dwm/home/.bashrc ~/.bashrc
+mv -f ~/dwm/home/.config/dwm/config.h ~/.config/dwm/ && cd ~/.config/dwm/ && makepkg -sif --clean
+sudo rm -rf ~/.bashrc.bak
+cd
     
-    sudo bash -c "echo '58,18 * * * * sudo pacman -Syu --noconfirm && flatpak update -y && paru -Syu && yes J | sudo pacman -Scc' >> /var/spool/cron/root" && sudo systemctl stop cronie && sudo systemctl enable cronie.service && sudo systemctl start cronie.service
-    sudo bash -c "echo '58,18 * * * * /home/ramin/.local/bin/update-dwm' >> /var/spool/cron/$USER" && systemctl stop cronie && systemctl enable cronie.service && systemctl start cronie.service
+sudo bash -c "echo '58,18 * * * * sudo pacman -Syu --noconfirm && flatpak update -y && paru -Syu && yes J | sudo pacman -Scc' >> /var/spool/cron/root" && sudo systemctl stop cronie && sudo systemctl enable cronie.service && sudo systemctl start cronie.service
+sudo bash -c "echo '58,18 * * * * /home/ramin/.local/bin/update-dwm' >> /var/spool/cron/$USER" && systemctl stop cronie && systemctl enable cronie.service && systemctl start cronie.service
 
-    source ~/.bashrc
-    source ~/.bash_profile
-fi
+source ~/.bashrc
+source ~/.bash_profile
 
 reboot
