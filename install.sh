3feb2f324e3605f78c0fdbb8ab193b36d8623ad8
raminsamadi123 <ramin.samad+github@gmail.com>
2023-03-25 14:49:23 +0100

    automated update

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 aur=paru
 git clone https://aur.archlinux.org/paru-bin.git ~/.config/paru && cd ~/.config/paru/ && makepkg -sif –clean
 
-$aur -Syu --needed - < ~/dwm/packages.txt
-    
+$aur -Syu --needed - <~/dwm/packages.txt
+
 pip install neovim langdetect shell-gpt --user
 
-gem install neovim && sudo npm install -g neovim 
+gem install neovim && sudo npm install -g neovim
 sudo gpasswd -a $USER plugdev
 sudo systemctl enable --now libvirtd.service && sudo usermod -a -G libvirt $(whoami) && sudo systemctl restart libvirtd.service
 sudo virsh net-start default
@@ -23,27 +23,49 @@ sudo ufw enable && sudo systemctl enable --now ufw.service
 cd
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
-echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf && sudo sed -i 's/#NoProgressBar/ILoveCandy/g' /etc/pacman.conf && sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
 sudo sed -i 's/ls -Fls/lsd -Fla/g' ~/.bashrc && sudo sed -i 's/ls -aFh/lsd -aFh/g' ~/.bashrc && sed -i 's,/usr/bin/grep $GREP_OPTIONS,grep --color,g' ~/.bashrc
-    
+
 mkdir ~/GitHub
-git clone https://github.com/raminsamadi123/dwm ~/GitHub/
 
 git clone https://aur.archlinux.org/dwm.git ~/.config/dwm && cd ~/.config/dwm/ && makepkg -sif –clean
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-    
-mv -f ~/dwm/home/.config/dwm/config.h ~/.config/dwm/ && cd ~/.config/dwm/ && makepkg -sif --clean
+# Neovim
+rm -rf ~/.config/nvim/
+mv ~/GitHub/dwm/home/.config/nvim/ ~/.config/
+
+# Alacritty
+rm -rf ~/.config/alacritty/
+mv ~/GitHub/dwm/home/.config/alacritty/
+
+# BTop
+rm -rf ~/.config/btop/
+mv ~/GitHub/dwm/home/.config/btop/
+
+# Calcurse
+rm -rf ~/.config/calcurse/
+mv ~/GitHub/dwm/home/.config/calcurse/
+calcurse -i ~/.config/calcurse/raminsam05@gmail.com.ics
+calcurse -i ~/.config/calcurse/ramin.samadi@elev.ga.ntig.se.ics
+calcurse -i ~/.config/calcurse/addressbook#contacts@group.v.calendar.google.com.ics
+calcurse -i ~/.config/calcurse/MAKER SPACE_classroom111097032359149922796@group.calendar.google.com.ics
+
+# Dunst
+rm -rf ~/.config/dunst/
+mv ~/GitHub/dwm/home/.config/dunst/
+
+# DWM
+rm -rf ~/.config/dwm/config.h
+mv ~/dwm/home/.config/dwm/config.h ~/.config/dwm/ && cd ~/.config/dwm/ && makepkg -sif --clean
+
 mv -f ~/dwm/home/.local/bin/* ~/.local/bin/
 mv -f ~/dwm/home/.bashrc ~/
 mv -f ~/dwm/home/.bash_profile ~/
 mv -f ~/dwm/home/.xinitrc ~/
 sudo rm -rf ~/.bashrc.bak
 cd
-    
+
 sudo bash -c "echo '58,18 * * * * sudo pacman -Syu --noconfirm && flatpak update -y && paru -Syu && yes J | sudo pacman -Scc' >> /var/spool/cron/root" && sudo systemctl stop cronie && sudo systemctl enable cronie.service && sudo systemctl start cronie.service
 sudo bash -c "echo '58,18 * * * * /home/ramin/.local/bin/update-dwm' >> /var/spool/cron/$USER" && systemctl stop cronie && systemctl enable cronie.service && systemctl start cronie.service
 
