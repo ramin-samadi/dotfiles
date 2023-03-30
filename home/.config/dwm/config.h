87c366bc8f40eb3b88524b3ab8508027349ba318
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-03-30 18:58:04 +0200

    automated update

--- home/.config/dwm/config.h
+++ home/.config/dwm/config.h
@@ -2,7 +2,7 @@
 
 // ▄▀█ █▀█ █▀█ █▀▀ ▄▀█ █▀█ ▄▀█ █▄░█ █▀▀ █▀▀
 // █▀█ █▀▀ █▀▀ ██▄ █▀█ █▀▄ █▀█ █░▀█ █▄▄ ██▄
-static const unsigned int borderpx  = 8;        /* border pixel of windows */
+static const unsigned int borderpx  = 12;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -12,11 +12,11 @@ static const char col_gray1[]       = "#1E1E2E";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#313244";
+static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
-  [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-  [SchemeSel] =  { col_gray4, col_cyan, col_gray1 },
+  [SchemeNorm] = { col_gray2, col_gray1, col_cyan },
+  [SchemeSel] =  { col_gray3, col_gray1, col_cyan },
 };
 
 // ▀█▀ ▄▀█ █▀▀ █▀▀ █ █▄░█ █▀▀
