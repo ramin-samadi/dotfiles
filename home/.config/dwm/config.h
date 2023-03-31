21d92a593143249625a0fff0d435f601833a9b60
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-03-31 11:18:05 +0200

    automated update

--- home/.config/dwm/config.h
+++ home/.config/dwm/config.h
@@ -11,12 +11,12 @@ static const char dmenufont[]       = "Meslo LGS Nerd Font Mono:size=20:bold";
 static const char col_gray1[]       = "#1E1E2E";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#313244";
 static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray2, col_gray1, col_cyan },
-  [SchemeSel] =  { col_gray3, col_gray1, col_cyan },
+  [SchemeSel] =  { col_gray3, col_gray4, col_cyan },
 };
 
 // ▀█▀ ▄▀█ █▀▀ █▀▀ █ █▄░█ █▀▀
