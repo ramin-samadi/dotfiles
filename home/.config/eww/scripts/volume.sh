f57d0f0bf78f3c4bc2a8a04598fab13c010e9364
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-04-06 23:58:05 +0200

    automated update

--- home/.config/eww/scripts/volume.sh
+++ home/.config/eww/scripts/volume.sh
@@ -1,26 +1,4 @@
 #!/usr/bin/env bash
 
-VOLUME_STATE=$(pamixer --get-volume)
-
-echo "${VOLUME_STATE}"
-
-if [[ $1 == "set" ]]; then
-  increase_volume="XF86AudioRaiseVolume"
-  decrease_volume="XF86AudioLowerVolume"
-
-  if ! pamixer --increase $increase_volume; then
-    echo "Error increasing volume"
-  fi
-
-  if ! pamixer --decrease $decrease_volume; then
-    echo "Error decreasing volume"
-  fi
-
-  if ! xdotool key $increase_volume; then
-    echo "Error simulating key press for increasing volume"
-  fi
-
-  if ! xdotool key $decrease_volume; then
-    echo "Error simulating key press for decreasing volume"
-  fi
-fi
+pamixer --set-volume "$2"
+echo $(pamixer --get-volume)
