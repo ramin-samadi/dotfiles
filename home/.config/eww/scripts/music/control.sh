58fb8aeb30b5c2b210041f33185a580e5e459ef0
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-04-12 01:58:05 +0200

    automated update

--- home/.config/eww/scripts/music/control.sh
+++ home/.config/eww/scripts/music/control.sh
@@ -4,8 +4,16 @@ if [[ $1 == "prev" ]]; then
   playerctl previous
 fi
 
+if [[ $1 == "backward" ]]; then
+  playerctl position $(echo $(playerctl position) - 10 | bc)
+fi
+
 if [[ $1 == "play-pause" ]]; then
   playerctl play-pause
+fi
+
+if [[ $1 == "forward" ]]; then
+  playerctl position $(echo $(playerctl position) + 10 | bc)
 fi
 
 if [[ $1 == "next" ]]; then
