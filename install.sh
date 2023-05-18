6c32326634287096486c21569d91a37dc80f1ee6
Ramin Samadi <ramin.samadi+github@elev.ga.ntig.se>
2023-05-18 10:05:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://aur.archlinux.org/paru-bin.git ~/.config/paru && cd ~/.config/paru/ && makepkg -sif –clean
+git clone https://aur.archlinux.org/paru-bin.git ~/.config/paru && cd ~/.config/paru/ && makepkg -sif -clean
 
 paru -Syu --needed - <"$HOME/dotfiles/packages.txt"
 
